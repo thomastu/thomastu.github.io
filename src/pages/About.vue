3b458a6d9b0ae09cec52c87651736b8f5982767a
Thomas Tu <thomasthetu@gmail.com>
2019-06-11 00:08:42 -0700

    try out gridsome

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>About Me</h1>
+    <p>Software engineer excited about energy efficiency, distributed energy resources, and renewables. Currently at Energy Solutions using python/Django to implement claim processing platforms for efficiency programs, and supporting internal BI efforts. Former software developer for the Energy Efficiency Standards group at Lawrence Berkeley National Lab where I worked on the International Database for Efficienct Appliances and internal tooling which facilitated the data collection, management and analysis of online appliance markets. Graduated in 2014 from UC San Diego with a B.S. in Physics and Mathematics.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <FullheightHero>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    
+    <h1>Hello, world!</h1>
+   
+    <p>
+      Software engineer excited about energy efficiency, distributed energy resources, and renewables. Currently at Energy Solutions using python/Django to implement claim processing platforms for efficiency programs, and supporting internal BI efforts. Former software developer for the Energy Efficiency Standards group at Lawrence Berkeley National Lab where I worked on the International Database for Efficienct Appliances and internal tooling which facilitated the data collection, management and analysis of online appliance markets. Graduated in 2014 from UC San Diego with a B.S. in Physics and Mathematics.
+    </p>
+  </FullheightHero>
+</template>
+
+<script>
+import FullheightHero from '~/layouts/Home.vue'
+
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  },
+  components: {
+    FullheightHero
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
