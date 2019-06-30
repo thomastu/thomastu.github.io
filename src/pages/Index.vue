918f07814b4e3f1c9eed089027eaa53b56d8253a
Thomas Tu <thomasthetu@gmail.com>
2019-06-29 20:08:17 -0700

    remove bad profile img

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <section class="section columns">
       <div class="container column is-half content">
-        <g-image alt="Example image" src="~/profile.png" height="200" width="200" style="border-radius: 50%;" />
+        <g-image alt="me" src="~/profile.png" height="200" width="200" style="border-radius: 50%;" />
         <p class="block">
           I'm currently over-engineering a new personal site!
           Stay tuned!
